92dd12bcf3d722b4d32ed260b06028a0bbeb3929
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-11 22:57:22 +0000

    Deploy tokio-rs/tokio to github.com/tokio-rs/tokio.git:gh-pages

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tokio"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"tokio_executor/enter/struct.EnterError.html\" title=\"struct tokio_executor::enter::EnterError\">EnterError</a>&gt; for <a class=\"struct\" href=\"tokio/executor/current_thread/struct.RunTimeoutError.html\" title=\"struct tokio::executor::current_thread::RunTimeoutError\">RunTimeoutError</a>",synthetic:false,types:["tokio::executor::current_thread::RunTimeoutError"]},{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"tokio_executor/enter/struct.EnterError.html\" title=\"struct tokio_executor::enter::EnterError\">EnterError</a>&gt; for <a class=\"struct\" href=\"tokio/executor/current_thread/struct.BlockError.html\" title=\"struct tokio::executor::current_thread::BlockError\">BlockError</a>&lt;T&gt;",synthetic:false,types:["tokio::executor::current_thread::BlockError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
